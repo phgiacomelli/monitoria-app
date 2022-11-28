bc35b91a9604810260cf2e6d5ed2c79faacd032e
Paulo Giacomelli <paulohenriquegiacomelli@gmail.com>
2022-11-28 01:02:17 -0300

    Updtaes

--- src/components/home/index.php
+++ src/components/home/index.php
@@ -11,12 +11,7 @@
   <link rel="stylesheet" href="../../../reset.css">
   <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="../../../globalStyles.css">
-<<<<<<< HEAD
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-=======
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
->>>>>>> 03d813ef53676dddc45503a5842c1ac190a21710
 
   <title>Monitoria-app | Home</title>
 </head>
@@ -25,28 +20,12 @@
   <div class="container">
     <div class="header">
       <div class="logo">
-<<<<<<< HEAD
-        <svg id="open-menu" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"
-          style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
-=======
         <svg id="open-menu" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
->>>>>>> 03d813ef53676dddc45503a5842c1ac190a21710
           <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
         </svg>
         <h1>Monitoria-app</h1>
       </div>
       <div class="user-opt">
-<<<<<<< HEAD
-        <p class="user">
-          <?php
-            echo $_SESSION['nome'];
-          ?>
-        </p>
-        <svg class="user" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"
-          style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
-          <path
-            d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z">
-=======
         <!-- <p>Paulo</p> -->
         <p>
           <?php
@@ -57,7 +36,6 @@
 
         <svg id="user-logo" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
           <path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z">
->>>>>>> 03d813ef53676dddc45503a5842c1ac190a21710
           </path>
         </svg>
 
@@ -76,9 +54,6 @@
       </div>
     </div>
     <div class="menu-bar">
-<<<<<<< HEAD
-
-=======
       <div class="menu-opt">
         a
         <ul>
@@ -90,7 +65,6 @@
           </li>
         </ul>
       </div>
->>>>>>> 03d813ef53676dddc45503a5842c1ac190a21710
     </div>
 
 
@@ -99,20 +73,6 @@
   <!-- JQUERY -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script>
-<<<<<<< HEAD
-     $('#open-menu').on("click", function () {
-      $('.menu-bar').toggleClass('expand');
-     });
-
-    $('.user').on("click", function () {
-      $('.opt-account').toggleClass('displayed');
-      console.log("b");
-    });
-  </script>
-</body>
-
-</html>
-=======
     $('#open-menu').on("click", function() {
       $('.menu-bar').toggleClass('expand');
     });
@@ -127,4 +87,3 @@
 </body>
 
 </html>
->>>>>>> 03d813ef53676dddc45503a5842c1ac190a21710
